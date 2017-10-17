6a1857f841e267f0f7785387b07f50cae45ceabe
7aRd1GrAd3 <ilya.sheidin@gmail.com>
2017-10-17 17:36:27 +0300

    Complete forms example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm; // Access local form referance
+  defaultQuestion = 'pet';
+  test = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    test: '',
+    gender: ''
+  };
+  submited = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+/*     this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      test: '',
+      gender: 'male'
+    }); */
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      }
+    });
+  }
+
+
+  /* onSubmit(form: NgForm) {
+    console.log(form);
+  } */
+  onSubmit() {
+    this.submited = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.test = this.signupForm.value.test;
+    this.user.gender = this.signupForm.value.gender;
+    console.log(this.signupForm);
+  }
+  onReset() {
+    this.signupForm.reset();
   }
 }
